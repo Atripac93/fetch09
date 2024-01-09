3a52c7350abd5dd512e76aacdaf8b5ee99145ba0
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-09 17:55:35 +0100

    Create cartella.js

---
+++ cartella.js
@@ -0,0 +1,19 @@
+let url = "https://api.pexels.com/v1/search?query=people";
+
+let container = document.querySelector(".foto");
+const fetchPrex = () => {
+  fetch(url, {
+    headers: {
+      Authorization: "2eq7QOEwouZD1HsRvIoalGyIjWxhIGYCMQaA2Fsk6PgRXu03DJMCr6xH",
+    },
+  }).then((response) => {
+    return response.json().then((data) => {
+      console.log(data);
+      const imageElements = data.photos.map((photo) => {
+        return `<img src="${photo.src.medium}" alt="${photo.photographer}"/>`;
+      });
+      container.innerHTML = imageElements.join("");
+    });
+  });
+};
+fetchPrex();
